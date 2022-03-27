af283c0520b406ff2e8f3b1075cb0008f40e7fdb
최윤제 <yoon@choeyunje-ui-MacBookAir.local>
2022-03-28 03:35:23 +0900

    fist commit

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -8,15 +8,20 @@
 
 import UIKit
 
+
 class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
-    }
+        
+        titleLabel.text = "⚡️FlashChat"
+        
+        
+        
     
-
+    }
 }
+
+
