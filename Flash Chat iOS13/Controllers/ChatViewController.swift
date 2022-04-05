b2f9ea8abfcac5bc987091c0e3d46b89dd290aec
최윤제 <yoon@choeyunje-ui-MacBookAir.local>
2022-04-05 15:58:57 +0900

    fisrt commit

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,23 +14,76 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
-    var meesages : [Message] = [
-        Message(sender: "1@2.com", body: "Hey!"),
-        Message(sender: "a@b.com", body: "Hey!"),
-        Message(sender: "1@2.com", body: "Hey!")
-    ]
+    let db = Firestore.firestore()
+    
+    var messages : [Message] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //tableView.delegate = self
         tableView.dataSource = self
         title = "⚡️FlashChat"
         navigationItem.hidesBackButton = true
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)  // 프로토타입 cell 모양 등록
+        
+        loadMessages()
     }
+    func loadMessages(){
+        
+        
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { querySnapshot, error in
+            
+            self.messages = []
+            
+            if let e = error{
+                print("There was an issue retrieveing data from Firestore. \(e)")
+            } else{
+                if let snapshotDocuments = querySnapshot?.documents {
+                    for doc in snapshotDocuments{
+                        let data = doc.data()
+                        if let messageSender = data[K.FStore.senderField] as? String, let messageBody = data[K.FStore.bodyField] as? String{
+                                let newMessage = Message(sender: messageSender, body: messageBody)
+                                self.messages.append(newMessage)
+        
+                                DispatchQueue.main.async {
+                                    self.tableView.reloadData()
+                                    let indexPath = IndexPath(row: self.messages.count - 1, section: 0 )  //section은 테이블을 나누는 구역
+                                    self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
+                            }
+                        }
+                    }
+                }
+                    
+            }
+        }
+    }
+    
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender,
+                                                                      K.FStore.bodyField: messageBody,
+                                                                      K.FStore.dateField: Date().timeIntervalSince1970]) { error in
+                if let e = error{
+                    print("There was an issue saving data to firestore")
+                }
+                    else {
+                        print("Successfully saved data.")
+                        
+                        DispatchQueue.main.async {
+                            self.messageTextfield.text = ""
+                        }
+                        
+                    }
+            }
+            
+        }
+        
     }
-    
+
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
         let firebaseAuth = Auth.auth()
     do {
@@ -41,26 +94,42 @@ class ChatViewController: UIViewController {
     }
       
     }
+     
+    
     
 }
 
 extension ChatViewController: UITableViewDataSource {
-    
+   
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return meesages.count
+        return messages.count
     }
-    
+    //각 셀별로 어떤값을 넣어줄지
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
-        //프로토타입셀을 가지고 몇개를 만들까
-        cell.textLabel?.text = meesages[indexPath.row].body  //셀 데이터를 요청하는 행 번호에 해당
+        
+        let message = messages[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell // MessageCell로 캐스팅해준다
+    
+        cell.label.text = message.body
+        
+        //현재 사용자의 메시지인지 messagecell 모양 변경
+        if message.sender == Auth.auth().currentUser?.email{
+            cell.leftImageView.isHidden = true
+            cell.rightImageView.isHidden = false
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.lightPurple)
+            cell.label.textColor = UIColor(named: K.BrandColors.purple)
+        } else{
+            cell.leftImageView.isHidden = false
+            cell.rightImageView.isHidden = true
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.purple)
+            cell.label.textColor = UIColor(named: K.BrandColors.lightPurple)
+            
+        }
+
         return cell
     }
+
 }
 
-//extension ChatViewController: UITableViewDelegate {
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print(indexPath.row)
-//    }
-//
-//}
+
+    
--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -13,6 +13,15 @@ class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     
+    override func viewWillAppear(_ animated: Bool) { // 상위 클래스의 함수를 override할때느 super도 호출 해주는게 좋다
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
